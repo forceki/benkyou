4a719dd6f22ee96905f12b0cc0b21ce96cf40729
forceki <forcekidesu@gmail.com>
2021-02-28 09:57:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	 
+	$('a.home').click(function(){
+		$('#home').removeClass('none');
+		$('#kanji').addClass('none');
+		$('#vocab').addClass('none');
+		$('#partikel').addClass('none');
+	});
+	$('a.kanji').click(function(){
+		$('#kanji').removeClass('none');
+		$('#home').addClass('none');
+		$('#vocab').addClass('none');
+		$('#partikel').addClass('none');
+	});
+	$('a.vocab').click(function(){
+		$('#vocab').removeClass('none');
+		$('#kanji').addClass('none');
+		$('#home').addClass('none');
+		$('#partikel').addClass('none');
+	});
+	$('a.partikel').click(function(){
+		$('#partikel').removeClass('none');
+		$('#kanji').addClass('none');
+		$('#home').addClass('none');
+		$('#vocab').addClass('none');
+	});
+});
